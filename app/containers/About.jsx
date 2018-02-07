4a1472ab49aa48617491493769e35ff529538925
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2018-02-07 15:38:05 +0000

    about info

--- app/containers/About.jsx
+++ app/containers/About.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const About = () => (
-  <div>Hello</div>
+  <div>
+    <h3>Built using following stack, lets call it SERN++ :)</h3>
+    <ul>
+      <li><a href="http://docs.sequelizejs.com/"><strong>S</strong>equelize</a> : Supports (MySql, MSSql, Postgres, Sqlite) databases</li>
+      <li><a href="http://expressjs.com"><strong>E</strong>xpress.js</a></li>
+      <li><a href="https://reactjs.org/"><strong>R</strong>eact</a></li>
+      <li><a href="https://nodejs.org"><strong>N</strong>ode.js</a></li>
+      <li><a href="https://webpack.js.org/">Webpack</a></li>
+      <li><a href="http://www.getbootstrap.com">Bootstrap</a></li>
+    </ul>
+  </div>
 );
 
 export default About;
--- app/containers/Home.jsx
+++ app/containers/Home.jsx
@@ -1,33 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => (
   <div>
     <div className="jumbotron text-center">
       <h1>React fullstack</h1> A Single Page Application built using React and
       Nodejs
-    </div>
-    <div className="row">
-      <div className="col-md-6">
-        <h4>React</h4>
-        <p>A JavaScript library for building user interfaces</p>
-        <p>
-          <a className="btn btn-info" href="https://reactjs.org/">
-            More info »
-          </a>
-        </p>
-      </div>
-      <div className="col-md-6">
-        <h4>NodeJs</h4>
-        <p>
-          Node.js® is a JavaScript runtime built on Chrome&quote;s V8 JavaScript
-          engine
-        </p>
-        <p>
-          <a className="btn btn-info" href="https://nodejs.org/en/">
-            More info »
-          </a>
-        </p>
-      </div>
+      <p>
+        <NavLink to="about" className="btn btn-info">More info</NavLink>
+      </p>
     </div>
   </div>
 );
