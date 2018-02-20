63a0e15578568f5a8cca295d5bc5b2827179987a
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2018-02-20 16:53:37 +0000

    user role conflict

--- server/db/seeders/20171013151115-userrole.js
+++ server/db/seeders/20171013151115-userrole.js
@@ -1,17 +1,11 @@
 /* eslint-disable */
-
-const sequelize = require('sequelize');
-
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.sequelize.query('select * from "UserRole"', {})
-    .then((count) => {
-      if (!count[0][0]) {
-        return queryInterface.bulkInsert('UserRole', [
-          { userid: 1, roleid: 1 },
-          { userid: 2, roleid: 2 },
-        ]);
-      }
-    }),
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('UserRole', [
+      { userid: 1, roleid: 1 },
+      { userid: 2, roleid: 2 }
+    ]).catch(err => Promise.resolve());
+  },
 
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('UserRole', null, {}),
 };
