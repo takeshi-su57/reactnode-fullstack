8238317980e7106acfbb4f2174d6462a548941df
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2018-02-23 11:22:17 +0000

    fixing prod index be served from server rather static middleware

--- server/middlewares/addProdMiddlewares.js
+++ server/middlewares/addProdMiddlewares.js
@@ -6,7 +6,7 @@ const universal = require('./universal');
 module.exports = function addProdMiddlewares(app, options) {
   const publicPath = options.publicPath || '/';
   const outputPath = options.outputPath || path.resolve(process.cwd(), 'build');
-  app.use(publicPath, express.static(outputPath));
+  app.use(publicPath, express.static(outputPath, { index: false }));
 
   app.get('*', (req, res) => {
     fs.readFile(path.resolve(outputPath, 'index.html'), (err, file) => {
