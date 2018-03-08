88f1c76563cbb0164c1d65afb02130a8d01a74b3
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2018-03-08 17:06:58 +0000

    simplified config

--- internals/webpack/webpack.dev.babel.js
+++ internals/webpack/webpack.dev.babel.js
@@ -10,8 +10,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
 const logger = require('../../server/logger');
-const pkg = require(path.resolve(process.cwd(), 'package.json'));
-const { dllPlugin } = pkg;
 
 const plugins = [
   new webpack.HotModuleReplacementPlugin(), // Tell webpack we want hot reloading
@@ -24,15 +22,6 @@ const plugins = [
     failOnError: false, // show a warning when there is a circular dependency
   }),
 ];
-
-if (dllPlugin) {
-  glob.sync(`${dllPlugin.path}/*.dll.js`).forEach((dllPath) => {
-    plugins.push(new AddAssetHtmlPlugin({
-      filepath: dllPath,
-      includeSourcemap: false,
-    }));
-  });
-}
 
 module.exports = require('./webpack.base.babel')({
   // Add hot reloading in development
@@ -50,7 +39,7 @@ module.exports = require('./webpack.base.babel')({
   },
 
   // Add development plugins
-  plugins: dependencyHandlers().concat(plugins), // eslint-disable-line no-use-before-define
+  plugins: plugins, // eslint-disable-line no-use-before-define
 
   // Emit a source map for easier debugging
   // See https://webpack.js.org/configuration/devtool/#devtool
@@ -60,63 +49,3 @@ module.exports = require('./webpack.base.babel')({
     hints: false,
   },
 });
-
-/**
- * Select which plugins to use to optimize the bundle's handling of
- * third party dependencies.
- *
- * If there is a dllPlugin key on the project's package.json, the
- * Webpack DLL Plugin will be used.
- *
- */
-function dependencyHandlers() {
-  // Don't do anything during the DLL Build step
-  if (process.env.BUILDING_DLL) { return []; }
-
-  // If the package.json does not have a dllPlugin property, Throw the error
-  if (!dllPlugin) {
-    throw new Error('No dllPlugin config in package.json');
-  }
-
-  const dllPath = path.resolve(process.cwd(), dllPlugin.path || 'node_modules/reactnode-fullstack-dlls');
-
-  /**
-   * If DLLs aren't explicitly defined, we assume all production dependencies listed in package.json
-   * Reminder: You need to exclude any server side dependencies by listing them in dllConfig.exclude
-   */
-  if (!dllPlugin.dlls) {
-    const manifestPath = path.resolve(dllPath, 'reactBoilerplateDeps.json');
-
-    if (!fs.existsSync(manifestPath)) {
-      logger.error('The DLL manifest is missing. Please run `npm run build:dll`');
-      process.exit(0);
-    }
-
-    return [
-      new webpack.DllReferencePlugin({
-        context: process.cwd(),
-        manifest: require(manifestPath), // eslint-disable-line global-require
-      }),
-    ];
-  }
-
-  // If DLLs are explicitly defined, we automatically create a DLLReferencePlugin for each of them.
-  const dllManifests = Object.keys(dllPlugin.dlls).map((name) => path.join(dllPath, `/${name}.json`));
-
-  return dllManifests.map((manifestPath) => {
-    if (!fs.existsSync(path)) {
-      if (!fs.existsSync(manifestPath)) {
-        logger.error(`The following Webpack DLL manifest is missing: ${path.basename(manifestPath)}`);
-        logger.error(`Expected to find it in ${dllPath}`);
-        logger.error('Please run: npm run build:dll');
-
-        process.exit(0);
-      }
-    }
-
-    return new webpack.DllReferencePlugin({
-      context: process.cwd(),
-      manifest: require(manifestPath), // eslint-disable-line global-require
-    });
-  });
-}
